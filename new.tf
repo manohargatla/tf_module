cda546988674630bd978eff749708013833b115b
manohargatla <126169049+manohargatla@users.noreply.github.com>
2023-04-09 16:01:52 +0530

    Update new.tf

--- new.tf
+++ new.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "tf_module" {
-    source = "git::https://github.com/manohargatla/tf_module"
+# module "tf_module" {
+#     source = "git::https://github.com/manohargatla/tf_module"
   
-}
+# }
